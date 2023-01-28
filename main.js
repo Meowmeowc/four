f98a62c9c19d5560ca04d6ba47d7b6da7413abf2
Meowmeowc <lalachka26@mail.ru>
2023-01-28 16:22:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// let a = 10
+// let b = 5
+// if (a + b > 15) {
+// 	console.log('ДА')
+// } else {
+// 	console.log('НЕТ')
+// }
+
+let c = 10
+let d = 10
+if (c === d) {
+	console.log('true')
+} else {
+	console.log('false')
+}
